2c072676f2611d8cb87fbcf0de23f35de27fa0df
Mi14251 <mailme_57@yahoo.co.in>
2016-08-10 11:23:02 +0000

    added test

---
+++ Capgemini.DevOps.UnitTest/UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capgemini.DevOps.UnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTest
+    /// </summary>
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            Assert.AreEqual(5, 5);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+            Assert.AreEqual(7, 7);
+        }
+    }
+}
